a44bef2f1469e35244deeb84b6a4f2244b76b2e1
Tashiev Emir <tashievemir01@gmail.com>
2023-10-14 18:42:01 +0600

    finished 12 videos of react

--- src/App.js
+++ src/App.js
@@ -3,23 +3,54 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Emir Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='app-wrapper'/*className="App"*/>
+      <header className='header' /*className="App-header"*/>
+        <img src='https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fibjvdkw%2Fkorean-bikini%2F&psig=AOvVaw0sW7d7C7ObhoXK3qdmcI7L&ust=1697371674487000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCOiTvK_A9YEDFQAAAAAdAAAAABA9' alt=' ' height={100} width={100} />
       </header>
+      <nav className='nav'>
+        <div>
+          <a>Profile</a>
+        </div>
+        <div>
+          <a>Messages</a>
+        </div>
+        <div>
+          <a>News</a>
+        </div>
+        <div>
+          <a>Music</a>
+        </div>
+        <div>
+          <a>Settings</a>
+        </div>
+      </nav>
+      <div className='content'>
+        <div>
+        <img src='https://www.google.com/url?sa=i&url=https%3A%2F%2Fmedium.com%2Fajmarketing-blog%2Ftop-30-stunning-korean-models-on-instagram-best-of-2020-9ffda209af22&psig=AOvVaw0sW7d7C7ObhoXK3qdmcI7L&ust=1697371674487000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCOiTvK_A9YEDFQAAAAAdAAAAABAj' />
+        </div>
+        <div>
+          ava+desc
+          <img src='https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.tiktok.com%2Fdiscover%2Ffemale-korean-models&psig=AOvVaw0sW7d7C7ObhoXK3qdmcI7L&ust=1697371674487000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCOiTvK_A9YEDFQAAAAAdAAAAABAt'  />
+        </div>
+        <div>
+          My post
+          <div>
+            New post
+          </div>
+          <div>
+            <div>
+              post 1
+            </div>
+            <div>
+              post 2
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
-  );
+
+    );
 }
 
 export default App;

fd1af08afa57665dcd3246eddcc8ae3ae062d17d
Tashiev Emir <tashievemir01@gmail.com>
2023-10-17 19:01:33 +0600

    splitted to components

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header =()=>{
+    return <header className='header' /*className="App-header"*/>
+        <img src='https://i.pinimg.com/236x/00/e9/1f/00e91f67b7609a99c66b784125a59c9d.jpg' alt=' ' height={59} width={59} />
+    </header>
+}
+
+export default Header;

484cfa5263b69681226ec8feca5ea08ff73321d3
Tashiev Emir <tashievemir01@gmail.com>
2023-11-20 19:20:35 +0600

    some fix

--- src/components/Profile/MyPosts/MyPost.js
+++ src/components/Profile/MyPosts/MyPost.js
@@ -1,6 +1,7 @@
 import React from "react";
 import s from './MyPost.module.css'
 import Post from "./Post/Post.js";
+import { addPostActionCreator, updateNewPostActionCreator } from "../../../redux/state.js";
 
 
 const MyPost = (props) => {
@@ -11,19 +12,19 @@ const MyPost = (props) => {
 
   let addPost = () => {
     // let text = newPostElement.current.value;
-    props.dispatch( { type: 'ADD-POST' } );
+    props.dispatch( addPostActionCreator());
   }
 
   let onPostChange = () => {
     let text = newPostElement.current.value;
-    props.dispatch( { type: 'UPDATE-NEW-POST-TEXT', newText: text } );
+    props.dispatch( updateNewPostActionCreator(text) );
   }
 
     return( 
       <div className={s.postsBlock}>
         My post
         <div>
-          <textarea onChange={onPostChange} ref={newPostElement} /*value={props.newPostText}*//>
+          <textarea onChange={onPostChange} ref={newPostElement} value={props.newPostText}/>
         </div>
         <div>
           <button onClick={addPost}>Add post</button>
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,8 @@
+
+const ADD_POST = 'ADD-POST';
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+const UPDATE_NEW_MESSAGE_BODY = 'UPDATE-NEW-MESSAGE-BODY';
+
 let store = {
     _state: {
         profilePage:{
@@ -12,6 +17,9 @@ let store = {
                 {id:1, message:'Hi'},
                 {id:2, message:'Hey'},
                 {id:3, message:'Yo'}
+            ],
+            newMessageBody:[
+                
             ],
             dialogs: [
                 {id:1, name:'Dimych'},
@@ -52,17 +60,34 @@ let store = {
         this._callSubscriber(this._state);
     },
     dispatch(action) {
-        if(action.type === 'ADD-POST')
+        if(action.type === ADD_POST)
         {
             this._addPost();
         }
-        else if(action.type === 'UPDATE-NEW-POST-TEXT') 
+        else if(action.type === UPDATE_NEW_POST_TEXT) 
         {
             this._updateNewPostText(action.newText);
+        }
+        else if(action.type === UPDATE-NEW-MESSAGE-BODY)
+        {
+
         }
     }
 
 }
+
+export const addPostActionCreator = () => {
+    return {
+      type: ADD_POST
+    }
+  }
+  
+export const updateNewPostActionCreator = (text) => {
+    return {
+      type: UPDATE_NEW_POST_TEXT,
+      newText: text
+    }
+  }
 
   export default store;
   window.store = store;
